0483a96e41ba34194139fc5f21be3a5310f867d8
Vesna Munjiza <vesna@Vesnas-MBP.fritz.box>
2019-01-29 23:16:53 +0100

    db.js with spicedPg and registerUser module

---
+++ db.js
@@ -0,0 +1,15 @@
+const spicedPg = require("spiced-pg");
+var db;
+if (process.env.DATABASE_URL) {
+    db = spicedPg(process.env.DATABASE_URL);
+} else {
+    const { dbUser, dbPassword } = require("./secrets");
+    db = spicedPg(`postgres:${dbUser}:${dbPassword}@localhost:5432/social`);
+}
+
+module.exports.registerUser = (first, last, email, hash) => {
+    return db.query(
+        `INSERT INTO users (first, last, email, password) VALUES ($1, $2, $3, $4) RETURNING id`,
+        [first, last, email, hash]
+    );
+};
