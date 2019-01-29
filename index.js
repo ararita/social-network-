8233ccf62a920c4c571d61386d8ec9d5be52ecee
Vesna Munjiza <vesna@Vesnas-MacBook-Pro.local>
2019-01-29 17:10:27 +0100

    body-parser, xpress.static, cookie-session

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const compression = require('compression');
+const compression = require("compression");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 
 app.use(compression());
 
-if (process.env.NODE_ENV != 'production') {
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(
+    cookieSession({
+        secret: `I'm always angry.`,
+        maxAge: 1000 * 60 * 60 * 24 * 14
+    })
+);
+
+app.use(express.static(__dirname + "/public"));
+
+if (process.env.NODE_ENV != "production") {
     app.use(
-        '/bundle.js',
-        require('http-proxy-middleware')({
-            target: 'http://localhost:8081/'
+        "/bundle.js",
+        require("http-proxy-middleware")({
+            target: "http://localhost:8081/"
         })
     );
 } else {
-    app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
+    app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("*", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(8080, function() {
