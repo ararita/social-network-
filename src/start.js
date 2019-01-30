1b15b59f0cbfa4a740e213fad2e5c187c4c6e416
Vesna Munjiza <vesna@Vesnas-MacBook-Pro.local>
2019-01-30 18:19:54 +0100

    imported Logo

--- src/start.js
+++ src/start.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Welcome } from "./welcome";
+// import { Register } from "./register";
+import { Logo } from "./logo";
+
+let renderStuff;
+
+if (location.pathname == "/welcome") {
+    renderStuff = <Welcome />;
+} else {
+    renderStuff = <Logo />;
+}
 
 ReactDOM.render(
-    <HelloWorld />,
-    document.querySelector('main')
+    renderStuff,
+    document.querySelector("main")
+    //ovaj main je main iz htmla, to je root
 );
-
-function HelloWorld() {
-    return (
-        <div>Hello, World!</div>
-    );
-}
