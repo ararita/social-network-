79e351098d21bcee0e9500b7510baabc7a972f1c
Vesna Munjiza <vesna@Vesnas-MacBook-Pro.local>
2019-01-30 18:18:08 +0100

    register and login working

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require("./bcrypt");
 const db = require("./db");
-
+const csurf = require("csurf");
 const app = express();
 
 app.use(compression());
 
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         secret: `I'm always angry.`,
@@ -21,7 +17,14 @@ app.use(
     })
 );
 
-app.use(express.static(__dirname + "./public"));
+app.use(express.static(__dirname + "/public"));
+
+app.use(csurf());
+
+app.use(function(req, res, next) {
+    res.cookie("mytoken", req.csrfToken());
+    next();
+});
 
 if (process.env.NODE_ENV != "production") {
     app.use(
@@ -34,13 +37,6 @@ if (process.env.NODE_ENV != "production") {
     app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-//---------missing hashed password business--------------
-app.post("/register", (req, res) => {
-    bcrypt.hashPassword(req.body.password).then(hash => {
-        db.registerUser(req.body.first, req.body.last, req.body.email, hash);
-    });
-});
-
 //---server routes:
 
 app.get("/welcome", function(req, res) {
@@ -51,7 +47,58 @@ app.get("/welcome", function(req, res) {
     }
 });
 
-//this has to be at the nend
+app.post("/register", (req, res) => {
+    console.log("req.body: ", req.body);
+    bcrypt
+        .hashPassword(req.body.password)
+        .then(hash => {
+            return db.registerUser(
+                req.body.first,
+                req.body.last,
+                req.body.email,
+                hash
+            );
+        })
+        .then(dbResult => {
+            req.session.userId = dbResult.rows[0].id;
+            req.session.first = req.body.first;
+            req.session.last = req.body.last;
+
+            res.json({ success: true });
+        })
+        .catch(err => {
+            res.json({ success: false });
+            console.log(err);
+        });
+});
+
+app.post("/login", (req, res) => {
+    let userId;
+    db.getUserInfo(req.body.email)
+        .then(dbResult => {
+            req.session.email = dbResult.rows[0].email;
+            if (dbResult.rows[0]) {
+                userId = dbResult.rows[0].id;
+                return bcrypt.compare(
+                    req.body.password,
+                    dbResult.rows[0].password
+                );
+            } else {
+                req.session.userId = dbResult.rows[0].id;
+                res.json({ notRegistered: true });
+            }
+        })
+        .then(() => {
+            req.session.userId = userId;
+            res.json({ success: true });
+        })
+        .catch(err => {
+            res.json({ success: false });
+            console.log(err);
+        });
+});
+
+//this has to be at the end
 app.get("*", function(req, res) {
     if (!req.session.userId) {
         res.redirect("/welcome");
