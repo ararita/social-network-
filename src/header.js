27e0fd8397d4320566b8b28e9705a509a14439ff
ararita <munjizavesna@gmail.com>
2019-02-07 00:30:09 +0100

    component created

---
+++ src/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ProfilePic from "./profilepic";
+import Logo from "./logo";
+
+export default function Header(props) {
+    return (
+        <div>
+            <div className="header">
+                <div className="logo-box">
+                    <Logo />
+                </div>
+
+                <div>
+                    <ProfilePic
+                        className="profile-pic-small"
+                        showUploader={props.showUploader}
+                        profilePicUrl={props.profilePicUrl}
+                        updateProfileUrl={props.updateProfileUrl}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
