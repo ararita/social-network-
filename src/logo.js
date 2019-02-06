b08ab517273ab080683241f80f25fda719f64d4c
ararita <munjizavesna@gmail.com>
2019-02-07 00:24:23 +0100

    new logo img

--- src/logo.js
+++ src/logo.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export function Logo() {
-    return <img src="/logo.jpg" id="logo" />;
+export default function Logo(props) {
+    return (
+        <div className="header-logo">
+            <img className="logo" src="/logo.jpeg" />
+        </div>
+    );
 }
+
+// import React from "react";
+//
+// export function Logo(props) {
+//     return (
+//         <div className="header">
+//             <img className="header-image" src="/assets/logo.png" />
+//         </div>
+//     );
+// }
+//
+// export default Logo;
