761de953001a54b9588ec36edaa2ba8718d723d6
ararita <munjizavesna@gmail.com>
2019-02-16 15:12:55 +0100

    wall component in progress

--- src/wall.js
+++ src/wall.js
@@ -1,1 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import axios from "./axios";
+import { initSocket } from "./socket";
+import { addWallMessage } from ".actions";
+import ProfilePic from "./profilepic";
+
+class Wall extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const { posts } = this.props;
+
+        if (!posts) {
+            return null;
+        }
+    }
+}
